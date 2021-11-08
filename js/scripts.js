22ac68e2e77e96f511d84196624c8ee635e5c7e9
Kim Brannian <brannian@gmail.com>
2021-11-07 19:35:50 -0800

    rework mr. roboger's neighborhood

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
-$(document).ready(function() {
-  $('form#groceryList').submit(function(event) {
-    event.preventDefault();
-    //Add item from input to array
-    const item1=$("#item1").val();
-    const item2=$("#item2").val();
-    const item3=$("#item3").val();
-    const item4=$("#item4").val();
-    const item5=$("#item5").val();
-    const item6=$("#item6").val();
-    
-    let array = [item1,item2,item3,item4,item5,item6];
-    array.sort();
-    array.forEach(function(item) {
-      $("#outputlist").append("<li>" + item + "</li>");
-    });
-  
-    
+//Business Logic
 
-    
+let beep = [];
+
+
+
+for (i=0; i<=userInput; i++) {
+  let stringNumber = (i.toString().split(""))
+  if (stringNumber.includes("3")) {
+    beep.push("Won't you be my neighbor");
+  } else if (stringNumber.includes("2")) {
+    beep.push("Boop!");
+  } else if (stringNumber.includes("1")) {
+    beep.push("Beep!");
+  } else {
+    beep.push(i);
+  }
+}
+console.log(beep)
+
+
+//UI Logic
+
+$(document).ready(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    const userInput = $("#enterNumber").val();
+    let result = beep;
+    $("#robogersOutput").text(result);
   });
 });
+
+    
