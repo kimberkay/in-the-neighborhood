8465eaf6cd097905c12003f090d6c727cb957331
Kim Brannian <brannian@gmail.com>
2021-11-09 13:04:50 -0800

    debug scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,38 @@
 //Business Logic
-
-
-
-function beepBoop(number) { 
+/*function beepBoop(number){
   let beep = [];
-for (i=0; i<=number; i++) {
-  console.log(number);
-  let stringNumber = (i.toString().split(""))
+  for (i=0; i<=number; i++) {
+    let stringNumber = (i.toString().split(""))
+    if (stringNumber.includes("3")) {
+      beep.push("Won't you be my neighbor");
+    } else if (stringNumber.includes("2")) {
+      beep.push("Boop!");
+    } else if (stringNumber.includes("1")) {
+      beep.push("Beep!");
+    } else {
+      beep.push(i);
+    }
+  }};
+  console.log(beep);*/
+
+
+ function beepBoop(number) { 
+  let beep = [];
+  for (i=0; i<=number; i++) {
+  let stringNumber = (number.toString().split(""))
   if (stringNumber.includes("3")) {
     beep.push("Won't you be my neighbor");
+    console.log(beep);
   } else if (stringNumber.includes("2")) {
     beep.push("Boop!");
   } else if (stringNumber.includes("1")) {
     beep.push("Beep!");
   } else {
-    beep.push(i);
+    beep.push(number);
   };
   return beep;
 }
-}  
+} 
 
 
 
